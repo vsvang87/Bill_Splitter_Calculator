a8f65cefa8b3312def0c19e43724ba3db255fb55
vsvang87 <vsvang@gmail.com>
2022-12-08 11:57:47 -0500

    12/8/22, changes to error message, added check for number javascript

--- js/app.js
+++ js/app.js
@@ -6,18 +6,29 @@ const errorMessage = document.querySelectorAll(".error");
 const resetBtn = document.getElementById("reset-btn");
 
 submitBtn.addEventListener("click", () => {
-  if (billAmount.value === "") {
+  //checking for numbers
+  const inputValue = billAmount.value;
+  const personsValue = persons.value;
+  const pattern = /^[0-9]*$/g;
+
+  //if input value does not match a digit
+  if (!inputValue.match(pattern)) {
     errorMessage[0].classList.add("active");
-  } else if (persons.value === "") {
-    errorMessage[1].classList.add("active");
-  } else if (billAmount.value != "" && persons.value != "") {
+  } else if (inputValue.match(pattern)) {
     errorMessage[0].classList.remove("active");
+  }
+  if (!personsValue.match(pattern)) {
+    errorMessage[1].classList.add("active");
+  } else if (personsValue.match(pattern)) {
     errorMessage[1].classList.remove("active");
-    let totalAmount = billAmount.value / persons.value;
+  }
+  //if both value match the digit pattern then calculate the value
+  if (inputValue.match(pattern) && personsValue.match(pattern)) {
+    let totalAmount = inputValue / personsValue;
     result.innerHTML = totalAmount;
   }
 });
-
+//reset all input field to zero
 resetBtn.addEventListener("click", () => {
   result.innerHTML = "0";
   billAmount.value = "";
