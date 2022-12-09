ce9f2e9eae0d3e8760c9c40c07bab24e50b94c83
vsvang87 <vsvang@gmail.com>
2022-12-09 10:28:35 -0500

    12/9/22, added check for special character

--- js/app.js
+++ js/app.js
@@ -6,10 +6,10 @@ const errorMessage = document.querySelectorAll(".error");
 const resetBtn = document.getElementById("reset-btn");
 
 submitBtn.addEventListener("click", () => {
-  //checking for numbers
+  //checking for numbers and special characters
   const inputValue = billAmount.value;
   const personsValue = persons.value;
-  const pattern = /^[0-9]*$/g;
+  const pattern = /^[0-9.]*$/g;
 
   //if input value does not match a digit
   if (!inputValue.match(pattern)) {
